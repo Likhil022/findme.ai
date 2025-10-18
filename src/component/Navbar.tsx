297a966d7d0d6ceecabc91f1873a110585353bc0
Likhil kosuru <likhilkosuru@MacBook-Air.local>
2025-10-18 10:36:27 +0530

    Inital Commit

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="bg-white-900 bg-white text-black p-4 flex justify-between px-20">
+      <h3 className="text-blue-600 text-2xl cursor-pointer">findme.ai</h3>
+      <ul className="flex gap-10 text-black">
+        <li>Home</li>
+        <li>About</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
